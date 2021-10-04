af6aa7e1a6f218bd954e8cbf269f3404c74453fa
aika823 <aika823@naver.com>
2021-10-04 10:33:38 +0900

    added pymysql on requirements

--- static/js/social_login.js
+++
@@ -1,7 +0,0 @@
-var naver_id_login = new naver_id_login("WO73y3DTPypJ9B7qq56N", "http://billim.co.kr/login/social/naver/callback/");
-var state = naver_id_login.getUniqState();
-naver_id_login.setButton("white", 2,40);
-naver_id_login.setDomain("http://billim.co.kr");
-naver_id_login.setState(state);
-naver_id_login.setPopup();
-naver_id_login.init_naver_id_login();
