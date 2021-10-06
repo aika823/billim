2a6b155e4f9f2f37e0e9fb01e0618a072fc9bd5b
aika823 <aika823@naver.com>
2021-10-06 11:52:56 +0900

    added secret.json

--- billim/settings.py
+++ billim/settings.py
@@ -3,8 +3,7 @@ from pathlib import Path
 import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -158,25 +157,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-print(STATICFILES_DIRS)
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AWS
-# SECRET_DIR = os.path.join(ROOT_DIR, 'secret')
+# SECRET
+SECRET_DIR = os.path.join(ROOT_DIR, 'secret')
+secrets = json.load(open(os.path.join(SECRET_DIR, 'secret.json'), 'rb'))
 
-PROJECT_ROOT    = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_DIR      = os.path.join(PROJECT_ROOT, 'secret')
-secretssssss = json.load(open(os.path.join(SECRET_DIR, 'secret.json'), 'rb'))
-
-# secret_file = 'secret.json'
-# with open(secret_file) as f:
-    # secrets = json.loads(f.read())
-
-AWS_ACCESS_KEY_ID = secretssssss['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = secretssssss['AWS_SECRET_ACCESS_KEY']
-
+# AWS SETTINGS
+AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
 AWS_REGION = 'ap-northeast-2'
 AWS_STORAGE_BUCKET_NAME = 'static.billim.co.kr'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME,AWS_REGION)
@@ -186,4 +177,3 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # MEDIA
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
